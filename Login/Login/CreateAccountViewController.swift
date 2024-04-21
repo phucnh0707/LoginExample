08ab0efa3df8c1288c086243bab352452e96f7a0
phucnh0707 <nguyenhphuc0707@gmail.com>
2024-04-21 06:37:54 -0700

    commit

--- Login/Login/CreateAccountViewController.swift
+++ Login/Login/CreateAccountViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateAccountViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func signUpClick(_ sender: UIButton) {
+        
+        if emailTextField.text!.isEmpty == true{
+            print("No text in email field")
+            return
+        }
+        
+        if passwordTextField.text!.isEmpty == true {
+            print("No text in password field")
+            return
+        }
+        
+        signUp()
+        
+        
     }
-    */
+    
+    @IBAction func alreadyHaveAnAccount(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "login")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
+    func signUp() {
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authResult, error) in
+            guard let user = authResult?.user, error == nil else{
+                print("Error \(error?.localizedDescription)")
+                return
+            }
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "mainHome")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true, completion: nil)
+            
+        }
+    }
 
 }
--- Login/Login/LoginViewController.swift
+++ Login/Login/LoginViewController.swift
@@ -6,24 +6,69 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+//    
+//    override func viewDidAppear(_ animated: Bool) {
+//        checkUserInfo()
+//    }
+//    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginClicked(_ sender: UIButton) {
+        validataFields()
     }
-    */
+    
+    @IBAction func createClicked(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "signUp")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true, completion: nil)
+    
+            
+    }
+    
+    func validataFields(){
+        if emailTextField.text!.isEmpty == true{
+            print("Email no text")
+            return
+        }
+        
+        if passwordTextField.text!.isEmpty{
+            print("Password no text")
+            return
+        }
+        login()
+    }
+    
+    func login()  {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {  [weak self] authResult, error in
+            guard let strongSelf = self else {return}
+            if let err = error{
+                print(err.localizedDescription)
+            }
+            self!.checkUserInfo()
+        }
+    }
+    func checkUserInfo() {
+        if Auth.auth().currentUser != nil{
+            print(Auth.auth().currentUser?.uid)
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "mainHome")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true, completion: nil)
+    }
+    
 
 }
